3866ebac8ca5627aa0c6db21c897063a21a80d7d
Jesús Aparicio <jesus.aparicio.fernandez.91@gmail.com>
2021-04-15 12:22:15 +0200

    subiendo a heroku

--- index.js
+++ index.js
@@ -24,17 +24,27 @@ mongoose.connect(MONGODB_URL, {useCreateIndex:true, useNewUrlParser:true, useUni
     app.use(router)
     app.use(guideRoute)
     app.use(placesRouter)
-    app.use(express.static(path.join(__dirname, "client", "build")))
+    app.use("*/storage", express.static("storage"));
 
-     app.use("*/storage", express.static("storage"));
+    if(process.env.NODE_ENV === "production"){
+      
+        app.use(express.static(path.join(__dirname, '/client/build')))
+
+        app.get("*", (req, res) => {
+           res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+       });
+    }else{
+        app.get('/', (req, res)=>{
+            res.send('Api running');
+        })
+    }
+
+     
     
    
    
    
     
-     app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
     
    
 
